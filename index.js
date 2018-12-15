77e5b7bb1a81989edd49fa4535b0cd8286d73a2d
Mike Wong <wongmdesign@gmail.com>
2018-12-14 21:57:33 -0500

    completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const app = express()
 
+// session middleware
 app.use(
   session({
     // this mandatory configuration ensures that session IDs are not predictable
@@ -12,7 +13,20 @@ app.use(
     saveUninitialized: true
   })
 )
+// session logging middleware
+app.use((req, res, next) => {
+  console.log('SESSION: ', req.session)
+  next()
+})
 
+// counter exercise
+app.use((req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0
+  console.log('counter', ++req.session.counter) // increment THEN log
+  next() // needed to continue to through express middleware
+})
+
+// response
 app.get('/', (req, res, next) => {
   res.send('Hello')
 })
